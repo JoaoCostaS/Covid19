1ca81fddfa150cb0d0e478c53e34f1d3159ff182
João Santos <joaoo.santoss.98@gmail.com>
2021-06-07 12:37:25 +0100

    criada class BdOpenHelper

---
+++ app/src/main/java/com/example/covid19/BdCovidOpenHelper.kt
@@ -0,0 +1,20 @@
+package com.example.covid19
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class BdCovidOpenHelper(context: Context?, )
+    : SQLiteOpenHelper(context, NOME_BASE_DADOS, null, VERSAO_BASE_DADOS) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+    companion object{
+        const val NOME_BASE_DADOS = "Covid.db"
+        const val VERSAO_BASE_DADOS = 1
+    }
+}
