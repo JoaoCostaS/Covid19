0e1086428787aeebc94aba7401cf6f83a0f74931
João Santos <joaoo.santoss.98@gmail.com>
2021-06-30 17:44:09 +0100

    criado layout para mostrar cada uma das cidades

--- app/src/main/java/com/example/covid19/ListaCidadesFragment.kt
+++ app/src/main/java/com/example/covid19/ListaCidadesFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
 
 // TODO: Rename parameter arguments, choose names that match
@@ -59,7 +60,13 @@ class ListaCidadesFragment : Fragment(),  LoaderManager.LoaderCallbacks<Cursor>{
      * @return Return a new Loader instance that is ready to start loading.
      */
     override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
-        TODO("Not yet implemented")
+        return CursorLoader(
+            requireContext(),
+            ContentProviderCovid.ENDERECO_CIDADES,
+            TabelaCidades.TODAS_COLUNAS,
+            null, null,
+            TabelaCidades.CAMPO_NOME
+        )
     }
 
     /**
